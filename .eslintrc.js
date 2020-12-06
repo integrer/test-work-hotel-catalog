2c5e6f7c30db7650b4966076d40019511962a0cb
integrer <osbib2@gmail.com>
2020-12-06 22:06:09 +0300

    Update package configuration

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+const extensions = ['.js', '.ts', '.vue', '.sass', '.scss', '.json'];
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:jsdoc/recommended',
+    'prettier',
+    'prettier/vue',
+    'prettier/@typescript-eslint',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['vue', 'jsdoc', 'prettier', 'import', '@typescript-eslint'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
+  settings: {
+    'import/extensions': extensions,
+    'import/resolver': {
+      alias: { extensions },
+      node: { extensions },
+      typescript: { alwaysTryTypes: true },
+    },
+  },
+  rules: {
+    'vue/max-attributes-per-line': 1,
+    'object-curly-spacing': [2, 'always'],
+    indent: ['warn', 2, { VariableDeclarator: 'first' }],
+    'comma-dangle': ['warn', 'always-multiline'],
+    quotes: ['warn', 'single'],
+  },
+};

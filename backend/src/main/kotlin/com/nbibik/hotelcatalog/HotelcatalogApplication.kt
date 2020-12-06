205f676de7604bd99d689eb61ff4677179c76aed
integrer <osbib2@gmail.com>
2020-12-06 23:03:49 +0300

    Add backend module

---
+++ backend/src/main/kotlin/com/nbibik/hotelcatalog/HotelcatalogApplication.kt
@@ -0,0 +1,11 @@
+package com.nbibik.hotelcatalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelcatalogApplication
+
+fun main(args: Array<String>) {
+    runApplication<HotelcatalogApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/nbibik/hotelcatalog/HotelcatalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nbibik.hotelcatalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelcatalogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

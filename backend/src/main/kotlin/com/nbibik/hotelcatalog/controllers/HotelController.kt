5efa4ade05248f3cb62c5c5a8be39b79565f2d7b
integrer <osbib2@gmail.com>
2020-12-16 10:10:31 +0300

    Add params to hotel action

--- backend/src/main/kotlin/com/nbibik/hotelcatalog/controllers/HotelController.kt
+++ backend/src/main/kotlin/com/nbibik/hotelcatalog/controllers/HotelController.kt
@@ -1,10 +1,10 @@
 package com.nbibik.hotelcatalog.controllers
 
-import com.nbibik.hotelcatalog.dao.HotelRepository
 import com.nbibik.hotelcatalog.entity.Hotel
 import com.nbibik.hotelcatalog.service.HotelListService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,7 +12,16 @@ class HotelController {
     @Autowired
     lateinit var hotelListService: HotelListService
 
-    // TODO: Add request params
     @GetMapping("/hotels")
-    fun hotels(): Iterable<Hotel> = hotelListService.getList()
+    fun hotels(
+        @RequestParam(value = "country") country: String?,
+        @RequestParam(value = "types") types: Array<String>?,
+        @RequestParam(value = "stars") stars: Array<Byte>?,
+        @RequestParam(value = "minReviews") minReviews: Int?,
+        @RequestParam(value = "maxPrice") maxPrice: Float?,
+        @RequestParam(value = "page") page: Int?,
+    ): Iterable<Hotel> = hotelListService.getList(
+        HotelListService.Query(country, types?.asIterable(), stars?.asIterable(), minReviews, maxPrice),
+        HotelListService.PageConfig.of(page)
+    )
 }

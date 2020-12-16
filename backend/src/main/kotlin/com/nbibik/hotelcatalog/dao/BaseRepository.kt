1e923fe78a32d00ae9980cb59fe82625e68daed3
integrer <osbib2@gmail.com>
2020-12-16 10:10:06 +0300

    Add jvm default to default customizer implementation

--- backend/src/main/kotlin/com/nbibik/hotelcatalog/dao/BaseRepository.kt
+++ backend/src/main/kotlin/com/nbibik/hotelcatalog/dao/BaseRepository.kt
@@ -12,5 +12,6 @@ import java.io.Serializable
 @NoRepositoryBean
 interface BaseRepository<TEntity : BaseEntity, TPath : EntityPathBase<TEntity>, TId : Serializable> :
     JpaRepository<TEntity, TId>, QuerydslPredicateExecutor<TEntity>, QuerydslBinderCustomizer<TPath> {
+    @JvmDefault
     override fun customize(bindings: QuerydslBindings, root: TPath) {}
 }

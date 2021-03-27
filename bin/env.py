1ae35d9e1b16618b1d6597286d8ec668c9660ed2
integrer <osbib2@gmail.com>
2021-03-28 01:15:32 +0300

    Add override exist vars option

--- bin/env.py
+++ bin/env.py
@@ -1,10 +1,17 @@
-from os import path
-from dotenv.main import load_dotenv
+from os import path, environ
+from functools import reduce
+from typing import Dict, Optional, Set
+from dotenv.main import dotenv_values
 
 
-def configure(mode: str, location: str):
+def get_envs(mode: str, location: str) -> Dict[str, Optional[str]]:
     test_dotenv_files = [f'.env.{mode}.local', f'.env.{mode}', '.env.local', '.env']
-    for file in test_dotenv_files:
-        filepath = path.join(location, file)
-        if path.isfile(filepath):
-            load_dotenv(filepath)
+    paths = map(lambda file: path.join(location, file), test_dotenv_files)
+    var_sets = [dotenv_values(f_path) for f_path in paths if path.isfile(f_path)]
+    return reduce(lambda acc, cur: {**acc, **cur}, var_sets, {})
+
+
+def apply_envs(envs: Dict[str, Optional[str]], override=False):
+    for name, value in envs:
+        if (override or name not in environ) and value is not None:
+            environ[name] = value
